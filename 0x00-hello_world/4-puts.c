034a79063ad2b9ff805ac1687a3c43dbd939c077
BelamfadelAbderrahim <abderrbelamfadel883@gmail.com>
2023-07-13 06:46:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
-int main(void){
+/**
+* main - a program that prints a line with puts function
+* Return: 0(success)
+*/
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
