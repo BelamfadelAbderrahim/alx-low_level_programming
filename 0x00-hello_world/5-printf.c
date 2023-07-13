dee4cc9b0f7aa067620f09f9a66fadf3f9199e70
BelamfadelAbderrahim <abderrbelamfadel883@gmail.com>
2023-07-13 07:22:27 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
-* main - a program that prints a line with printf function
-* Return: 0 (success)
+ * main - a program that prints a line with printf function
+ * Return: 0 (success)
 */
 int main(void)
 {
